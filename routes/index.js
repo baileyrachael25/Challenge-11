897c8eb1571c9a5450b64fe70ea2c891c3cb2811
baileyrachael25 <baileyrachael25@gmail.com>
2022-12-05 15:58:23 -0800

    small starter file changes

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,30 @@
 //import dependencies
 const router = require('express').Router();
+const { notStrictEqual } = require('assert');
 const {randomUUID} = require('crypto');
 const { router } = require('express');
 const fs = require('fs');
 const path = require('path');
 
 //GET all notes route
+notes.get('/', (req, res) =>
 
+);
 
 //POST notes route
+notes.post('/', (req, res) => {
 
+
+});
 
 //DELETE note by id route
+notes.delete('/', (req, res) =>
 
+);
 
 //PUT route to update ntoe by id
+notes.put('/', (req, res) =>
 
+);
 
 module.exports = router;
